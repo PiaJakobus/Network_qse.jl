d08ea82f940fb1f271194858754bd34cb03f5246
Pia.Jakobus <pia.jakobus@gmx.de>
2020-07-07 11:26:46 +1000

    commit test in atom

--- src/Network_qse.jl
+++ src/Network_qse.jl
@@ -5,6 +5,7 @@ __precompile__(false)
 using CSV
 using DelimitedFiles
 using DataFrames
+using Roots
 
 include("Io.jl")
 include("Constants.jl")
@@ -41,17 +42,15 @@ function initial_partition_function()
     n_B = 1.0/const_m_B
     β   = 1.0/(const_kmev * 1.0)
     scr3 = 1. # seems odd in code, not set..later sum(x_i)
-    ω,A,Z,s,m = G[1:5] # careful dimensions G[1] isa 3 x 8 matrix
-    N = A - Z
+    ω,A,Z,s,m = G[1:5]
     root_T⁻¹ = .√(1.0./data_T)
-    fp₀ = map(om->(2.0.*s.+1.0)*om, ω)
+    #fp₀ = map(om->(2.0.*s.+1.0)*om, ω)
+    fp₀ = ω.*(2 .*s .+ 1)
     λ₀ = .√const_hh^2/(2.0*π*const_k_B*(A*const_m_B .+ m*const_kmev/const_c^2))
     λ = root_T⁻¹*λ₀
-    E_b =  m - Z*m[2] + N*m[1]
-    prefac = map(i->(A[i]*fp₀[i]*λ[i].^3.0)/n_B, 1:length(fp₀))
+    prefac = vcat(map(i->(A[i]*fp₀[i]*λ[i].^3.0)/n_B, 1:length(fp₀))...)
     return prefac
 end
-
 
 
 
@@ -75,6 +74,30 @@ end
 
 
 
+function saha_equation(μₙ,μₚ,T,ρ)
+    prefactor = initial_partition_function()
+    A, Z, m = G[[2,3,5]]
+    N = A - Z
+    E_b =  m - Z*m[2] + N*m[1]
+    xᵢ = exp.(μₚ .* z)
+    return [x^2, x^3]
+end
+
+μₚ = 1
+μₙ = 1
+T = 1
+A, Z, m = G[[2,3,5]]
+N = A - Z
+E_b =  m - Z*m[2] + N*m[1]
+pr = initial_partition_function()
+xᵢ = pr .* exp.((μₚ .* Z + μₙ .* N - E_b).*(const_k_B * T))
+Yₑ = xᵢ .* Z ./ A
+
+function find_root()
+    f(μ) = sum(saha_equation(μₙ,μₚ,T,ρ)) - 1
+    g(μ) = sum(charge_neutrality())
+    find_zeros(f,-∞, ∞)
+end
 
 
 
